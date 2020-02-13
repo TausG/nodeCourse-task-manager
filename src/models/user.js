36f8348800768e5dfba79e4d27411413eb34e1c7
Taus <tausg@hotmail.com>
2020-02-13 17:33:40 +0100

    refactor user model to use Schema and bcryptjs to hash password. refactor users routes so middleware gets called for patch

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -40,16 +41,16 @@ const User = mongoose.model('User', {
     }     
 });
 
-// const me = new User({
-//     name: 'Taus       ',
-//     email: '      taus@hotmail.COM',
-//     password: 'myPhone098!'
-// });
+userSchema.pre('save', async function(next) {
+    const user = this;
 
-// me.save().then((me) => {
-//     console.log(me);
-// }).catch((error) => {
-//     console.log('Error', error);
-// })
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
--- src/routers/users.js
+++ src/routers/users.js
@@ -40,7 +40,7 @@ router.get('/users/:id', async (req, res) => {
 });
 
 router.patch('/users/:id', async (req, res) => {
-    const updatesAllowed = ['name', 'age', 'email'];
+    const updatesAllowed = ['name', 'age', 'email', 'password'];
     const updates = Object.keys(req.body);
     const isUpdatesAllowed = updates.every((update) => updatesAllowed.includes(update));
 
@@ -49,7 +49,11 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-      const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        const user = await User.findById(req.params.id);
+
+        updates.forEach((update) => user[update] = req.body[update]);
+        await user.save();
+        //const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
 
         if (!user) {
             return res.status(404).send();
