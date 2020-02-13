0b9e4184a3df275265a46408d7c69209b2285349
Taus <tausg@hotmail.com>
2020-02-13 23:21:55 +0100

    added jwt authentication as middleware

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ const taskRouter = require('./routers/tasks');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('Get requests are disabled');
+//     } else {
+//         next();
+//     }
+// });
+
+// app.use((req, res, next) => {
+//     res.status(503).send('Server is currently down for maintenance, please try again later');
+// });
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
